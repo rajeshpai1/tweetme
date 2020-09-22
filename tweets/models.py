6cc5857ca23775b28c1f95f9f04782818db43390
rajeshpai1 <rajeshp0911@gmail.com>
2020-09-22 17:43:22 +0530

    first commit

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Tweets(models.Model):
+  content = models.TextFeild(blank = True, null= True)
+  image = models.FileFeild(uplaod_to='images', blank=True)
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import httpResponse, Http404, jsonResponse
 # Create your views here.
+
+def home_view(request):
+  return h
