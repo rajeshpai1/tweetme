5d61975bc647621b45d91e5ec8f4b47c6914eb38
rajeshpai1 <rajeshp0911@gmail.com>
2020-09-22 18:03:22 +0530

    testing autogit

--- tweets/models.py
+++ tweets/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Tweets(models.Model):
-  content = models.TextFeild(blank = True, null= True)
-  image = models.FileFeild(uplaod_to='images', blank=True)
+	content = models.TextFeild(blank=True, null=True)
+	image = models.FileFeild(uplaod_to='images', blank=True)
